393c67e9769d55542cb635b3544140ac7815c228
Bryce007 <clack.bryce@gmail.com>
2018-12-06 21:55:53 -0500

    initial commit

---
+++ src/test/java/com/maven/BaseClass.java
@@ -0,0 +1,31 @@
+package com.maven;
+
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.testng.annotations.Test;
+
+public class BaseClass {
+	
+	
+	
+	@Test()
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "/Users/Bryce/eclipse-workspace/SeleniumBasics/src/Drivers/chromedriver");
+		WebDriver driver=new ChromeDriver();
+		driver=new ChromeDriver();
+		driver.get("https://Opensource-Demo.Orangehrmlive.com/");
+		
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().fullscreen();
+		
+		
+	}
+
+	
+	}
+
---
+++ src/test/java/com/maven/MavenTest2.java
@@ -0,0 +1,13 @@
+package com.maven;
+
+import org.testng.annotations.Test;
+
+public class MavenTest2 {
+	
+	@Test
+	public void hello2() {
+		System.out.println("This is my second maven test");
+	}
+
+
+}
