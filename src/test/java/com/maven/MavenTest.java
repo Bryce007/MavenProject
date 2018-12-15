d96967f04e8b4d92fded9cc1be7ae054282ade01
Bryce007 <clack.bryce@gmail.com>
2018-12-14 21:22:49 -0500

    Gigi changes

--- src/test/java/com/maven/MavenTest.java
+++ src/test/java/com/maven/MavenTest.java
@@ -1,15 +1,14 @@
 package com.maven;
 
 import org.testng.annotations.Test;
-import org.testng.annotations.Test;
-import org.testng.annotations.Test;
-import org.testng.annotations.Test;
 
 public class MavenTest {
 	
 	@Test
 	public void hello() {
 		System.out.println("This is my first maven test");
+		
+		
 	}
 
 }
